7c66d51f2bad50eecab84d66980b49dfc53ffd23
Jhen <developer@jhen.me>
2017-12-01 13:54:12 +0800

    Move fixture to __tests__

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,19 +6,19 @@ test('yarn-workspace-sync-deps', () => {
   const matchSnaps = () => {
     expect(
       fs.readFileSync(
-        path.join(__dirname, '../fixture/test/test-package1/package.json'),
+        path.join(__dirname, 'fixture/test/test-package1/package.json'),
         'utf-8',
       ),
     ).toMatchSnapshot()
     expect(
       fs.readFileSync(
-        path.join(__dirname, '../fixture/test2/test2-package1/package.json'),
+        path.join(__dirname, 'fixture/test2/test2-package1/package.json'),
         'utf-8',
       ),
     ).toMatchSnapshot()
   }
 
-  shell.cd(path.join(__dirname, '../fixture/'))
+  shell.cd(path.join(__dirname, 'fixture/'))
   matchSnaps()
   shell.exec('yarn')
   matchSnaps()
