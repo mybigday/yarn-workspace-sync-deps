c65817175e5fd51e77b534720e8946b3f188cbba
Matt Morrissette <yinzara@gmail.com>
2019-01-09 21:19:24 -0800

    add support for non-wildcard workspaces

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -30,3 +30,38 @@ test('yarn-workspace-sync-deps', () => {
   matchSnaps()
   shell.exec('git checkout .')
 })
+
+test("yarn-workspace-sync-deps named workspaces with no wild cards", () => {
+  const matchSnaps = () => {
+    expect(
+        fs.readFileSync(
+            path.join(__dirname, 'fixture/test4/test4-package1/package.json'),
+            'utf-8',
+        ),
+    ).toMatchSnapshot()
+    expect(
+        fs.readFileSync(
+            path.join(__dirname, 'fixture/test4/test4-package2/package.json'),
+            'utf-8',
+        ),
+    ).toMatchSnapshot()
+    expect(
+        fs.readFileSync(
+            path.join(__dirname, 'fixture/test4/packages/test4-package3/package.json'),
+            'utf-8',
+        ),
+    ).toMatchSnapshot()
+    expect(
+        fs.readFileSync(
+            path.join(__dirname, 'fixture/test4/packages/test4-package4/package.json'),
+            'utf-8',
+        ),
+    ).toMatchSnapshot()
+  }
+
+  shell.cd(path.join(__dirname, 'fixture/test4'))
+  matchSnaps()
+  shell.exec('yarn')
+  matchSnaps()
+  shell.exec('git checkout .')
+})
